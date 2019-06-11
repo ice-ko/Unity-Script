cd7d0ddb418cfb8036e64015385c71688d76be35
L <823166623@qq.com>
2019-06-11 17:23:50 +0800

    遗漏文件

---
+++ 背包系统/Assets/Scripts/Base/EnumExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 枚举扩展类
+/// </summary>
+public static class EnumExtension
+{
+    /// <summary>
+    /// 获取枚举的描述信息
+    /// </summary>
+    public static string GetDescription(this Enum em)
+    {
+        Type type = em.GetType();
+        FieldInfo fd = type.GetField(em.ToString());
+        if (fd == null)
+            return string.Empty;
+        object[] attrs = fd.GetCustomAttributes(typeof(DescriptionAttribute), false);
+        string name = string.Empty;
+        foreach (DescriptionAttribute attr in attrs)
+        {
+            name = attr.Description;
+        }
+        return name;
+    }
+}
---
+++ 背包系统/Assets/Scripts/Inventory/ToolTip.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour
+{
+    public float smoothing = 1;//渐变平滑速度
+
+    private float tatgetAlpha = 0;
+    //
+    private Text toolTipText;
+    private Text contentText;
+    private CanvasGroup canvasGroup;
+    void Start()
+    {
+        toolTipText = GetComponent<Text>();
+        contentText = transform.Find("ContentText").GetComponent<Text>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    void Update()
+    {
+        if (canvasGroup.alpha != tatgetAlpha)
+        {
+            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, tatgetAlpha, smoothing * Time.deltaTime);
+            //是否到底目标值
+            if (Mathf.Abs(canvasGroup.alpha - tatgetAlpha) < 0.01f)
+            {
+                canvasGroup.alpha = tatgetAlpha;
+            }
+        }
+    }
+    /// <summary>
+    /// 显示提示框
+    /// </summary>
+    /// <param name="text"></param>
+    public void Show(string text)
+    {
+        toolTipText.text = text;
+        contentText.text = text;
+        tatgetAlpha = 1;
+    }
+    /// <summary>
+    /// 隐藏提示框
+    /// </summary>
+    public void Hide()
+    {
+        tatgetAlpha = 0;
+    }
+}
