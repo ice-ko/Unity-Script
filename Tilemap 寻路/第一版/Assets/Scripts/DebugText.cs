e0b5f2c962632abd47e15e7c2f2267fd9d96e018
L <823166623@qq.com>
2019-09-11 17:48:51 +0800

    第一版 代码混乱

---
+++ Tilemap 寻路/第一版/Assets/Scripts/DebugText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugText : MonoBehaviour
+{
+    public TextMeshProUGUI f, g, h, p;
+    public RectTransform arrow;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
