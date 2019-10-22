d11497fb5166ed2b1f22ef0e2ce2ae1093edc413
L <823166623@qq.com>
2019-10-22 17:40:07 +0800

    Socket服务端

---
+++ Socket服务器/GameServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Server;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerPeer serverPeer = new ServerPeer();
+            serverPeer.Start(9999,10);
+
+
+            Console.ReadKey();
+        }
+       
+    }
+}
